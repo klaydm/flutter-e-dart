9bb9f6361c15e0173765dd1e4bb087eb15dcc043
Kennedy Klaydm <klaydm@gmail.com>
2025-11-11 21:45:23 -0300

    Subindo projetos do estudo de Dart/Flutter

---
+++ dart/meu_app/bin/meu_app.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main() {
+  print("Olá, me chamo Dart, qual o seu nome?");
+  var entrada = stdin.readLineSync();
+  print("Muito prazer $entrada, vamos fazer muitos programas juntos!");
+}
+
+
